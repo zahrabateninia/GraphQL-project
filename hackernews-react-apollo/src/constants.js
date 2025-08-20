18586f97e0dcbd85dce957b9307727a494c37ab0
zahrabateninia <zahrabatenin@gmail.com>
2025-08-20 20:54:40 +0330

    Define the key for credentials to be stored in the browser's localStorage

---
+++ hackernews-react-apollo/src/constants.js
@@ -0,0 +1,3 @@
+// Storing JWTs in localStorage is NOT  a safe approach to implement authentication on the frontend. 
+// Because I want to keep things simple I am using it here.
+export const AUTH_TOKEN = 'auth-token';
