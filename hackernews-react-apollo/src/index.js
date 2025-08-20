2742cf79bfe661ae4ecc62256e38597cfc81e83f
zahrabateninia <zahrabatenin@gmail.com>
2025-08-20 17:02:49 +0330

    Install apollo client and configure it

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import './styles/index.css';
 import App from './components/App';
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+
+// run graphql server on http://localhost:4000
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+});
+
+// initiate apollo client
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
